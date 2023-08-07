3a629478b4aa8265a6497c9fff31d9750c8a3a79
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-08-07 10:06:34 +0530

    Day05

---
+++ Day05/DemoTest/src/com/tka/demo/basic/Demo1.java
@@ -0,0 +1,21 @@
+package com.tka.demo.basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+			
+		
+
+	}
+
+}
